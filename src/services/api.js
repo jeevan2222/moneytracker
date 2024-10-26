91bd26d3ecceaae8fba14a11deeecd936e2346c4
jeevanK <jeevankumark@zen3.com>
2024-10-26 21:20:59 +0530

    resolved cors erros

--- src/services/api.js
+++ src/services/api.js
@@ -4,25 +4,33 @@ const REACT_APP_BASE_URL = "https://expensive-lgia.onrender.com";
 
 export const signIn = async (data) => {
     try {
-        const response = await axios.post(`${REACT_APP_BASE_URL}/login`, data);
+        const response = await axios.post(`${REACT_APP_BASE_URL}/login`, data, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        });
         return response?.data;
 
     } catch (error) {
-      
-        return error.response.data;
-        console.log(error);
+        console.error(error);  // It's best to log the error first
+        return error.response?.data;
     }
 };
-export const getMoney = async (data) => {
 
+export const getMoney = async (data) => {
     console.log(data);
     try {
-        const response = await axios.post(`${REACT_APP_BASE_URL}/trackmoney`, data);
+        const response = await axios.post(`${REACT_APP_BASE_URL}/trackmoney`, data, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        });
         return response?.data;
 
     } catch (error) {
-      
-        return error.response.data;
-        console.log(error);
+        console.error(error);  
+        return error.response?.data;
     }
 };
